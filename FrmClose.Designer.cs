f95ed8e8af593f18675f8bb9917bd016319f1b3c
Fatemeh Sadat <104809077+musavi79@users.noreply.github.com>
2024-04-22 22:49:31 +0330

    Add files via upload

---
+++ FrmClose.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Farhang_Loqat
+{
+    partial class FrmClose
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmClose));
+            this.txtComment = new System.Windows.Forms.TextBox();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtComment
+            // 
+            this.txtComment.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtComment.Location = new System.Drawing.Point(32, 64);
+            this.txtComment.Multiline = true;
+            this.txtComment.Name = "txtComment";
+            this.txtComment.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtComment.Size = new System.Drawing.Size(504, 37);
+            this.txtComment.TabIndex = 0;
+            this.txtComment.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtComment.TextChanged += new System.EventHandler(this.txtComment_TextChanged);
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Font = new System.Drawing.Font("A Thuluth", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnAccept.Image = ((System.Drawing.Image)(resources.GetObject("btnAccept.Image")));
+            this.btnAccept.Location = new System.Drawing.Point(125, 339);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(110, 48);
+            this.btnAccept.TabIndex = 1;
+            this.btnAccept.Text = "ثبت";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("A Thuluth", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(9, 339);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 48);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "خروج";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmClose
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(570, 393);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.txtComment);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmClose";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "خروج از برنامه";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtComment;
+        public System.Windows.Forms.Button btnAccept;
+        public System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ FrmClose.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farhang_Loqat
+{
+    public partial class FrmClose : Form
+    {
+        public FrmClose()
+        {
+            InitializeComponent();
+        }
+        string matn = "";
+        Form1 frm1 = new Form1();
+       //textbox daryaft nazar
+        private void txtComment_TextChanged(object sender, EventArgs e)
+        {
+            matn = txtComment.Text;
+        }
+
+        //methode baraye sabt nazar
+        public void comment(string matn)
+        {
+            MailMessage n = new MailMessage();
+            n.From = new MailAddress("nazaratmail68@gmail.com", "DictionaryTest");
+            n.To.Add("sadatmusavi2000@gmail.com");
+            n.Subject = "گزارش";
+            n.Body = matn;
+            SmtpClient sc = new SmtpClient("smtp.gmail.com");
+            sc.Port = 587;
+            sc.Credentials = new NetworkCredential("nazaratmail68@gmail.com", "68nazaratmail68");
+            sc.EnableSsl = true;
+            sc.Send(n);
+
+        }
+        FrmError fe = new FrmError();
+        FrmAccept fa = new FrmAccept();
+        //button sabt nazar
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comment(matn);
+                fa.ShowDialog();
+                frmAssli();
+               
+
+            }
+            catch (Exception)
+            {
+               fe.ShowDialog();
+            }
+
+        }
+        //tabe baraye bastan form ha
+        public void frmAssli()
+        {
+            var form = Application.OpenForms["FrmAsli"];
+            var fform = Application.OpenForms["Form1"];
+            if (form == null || fform == null)
+            {
+                form = new FrmAsli();
+                fform = new Form1();
+                
+            }
+            form.Close();
+            fform.Close();
+
+        }
+        //button khorooj
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+            frmAssli();
+        }
+    }
+}
